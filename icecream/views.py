352bb6d22da6fdfcec45ed45411b5a0698ea0947
JustLike420 <justlikevova@gmail.com>
2020-10-26 08:55:11 +0300

    Display ice cream names

--- icecream/views.py
+++ icecream/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import models
+from . import models  # можно заменить на from .models import icecream_db
 
 def icecream_list(request):
     db_query = models.icecream_db # получаем данные: полный список сортов
-    icecreams = f'Список мороженного {db_query}'
+    icecream = ''
+    for i in range(len(db_query)):
+        icecream += f'{db_query[i]["name"]} :: '
+    icecreams = f'Список мороженного: {icecream}'
     return HttpResponse(icecreams)
