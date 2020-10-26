1ea03c672180fd5b3f954ba17d4b700ff693f522
JustLike420 <justlikevova@gmail.com>
2020-10-26 08:06:38 +0300

    Connected new app 'IceCream'

--- Anfisa/urls.py
+++ Anfisa/urls.py
@@ -7,4 +7,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
+    path('icecream/', include('icecream.urls'))
 ]
