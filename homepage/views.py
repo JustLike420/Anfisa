3a4d8c31bb60a2998ef1e2b4f756c4628067ef7f
JustLike420 <justlikevova@gmail.com>
2020-10-26 21:59:25 +0300

    Added templates and used render in views.py

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('Anfisa for Friends -- Hello, world! -- ¯\(°:°)/¯')
+    return render(request, 'homepage/index.html')
