35142497401a51c8c525f0b48d7d231d83625987
JustLike420 <justlikevova@gmail.com>
2020-10-28 13:13:26 +0300

    a separate page for each type of icecream

--- icecream/urls.py
+++ icecream/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.icecream_list),
+    path('<int:pk>/', views.icecreams_details),
 ]
--- icecream/views.py
+++ icecream/views.py
@@ -6,7 +6,16 @@ def icecream_list(request):
     db_query = models.icecream_db # получаем данные: полный список сортов
     icecreams = ''
     for i in range(len(db_query)):
-        icecreams += f'{db_query[i]["name"]} <br>'
+        icecreams += f'<a href="{i}/">{db_query[i]["name"]}</a><br>'
     context = {'icecreams': icecreams}
 
     return render(request, 'icecream/icecream-list.html', context)
+
+def icecreams_details(request, pk):
+    name = models.icecream_db[pk]['name']
+    description = models.icecream_db[pk]['description']
+    context = {
+        'name': name,
+        'description': description,
+    }
+    return render(request, 'icecream/icecream-details.html', context)
