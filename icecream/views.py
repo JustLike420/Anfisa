76aaa1cb65b48b982302d03c4cd7eca2bc1fea4f
JustLike420 <justlikevova@gmail.com>
2020-10-28 10:39:53 +0300

    render()

--- icecream/views.py
+++ icecream/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from . import models  # можно заменить на from .models import icecream_db
 
 def icecream_list(request):
     db_query = models.icecream_db # получаем данные: полный список сортов
-    icecream = ''
+    icecreams = ''
     for i in range(len(db_query)):
-        icecream += f'{db_query[i]["name"]} :: '
-    icecreams = f'Список мороженного: {icecream}'
-    return HttpResponse(icecreams)
+        icecreams += f'{db_query[i]["name"]}'
+    ice = {'icecream': icecreams}
+
+    return render(request, 'icecream/icecream-list.html', context=ice)
