d5939775c4cc4c9e50ffdd749317a64b77d94260
JustLike420 <justlikevova@gmail.com>
2020-10-28 10:48:37 +0300

    render(context)

--- icecream/views.py
+++ icecream/views.py
@@ -6,7 +6,7 @@ def icecream_list(request):
     db_query = models.icecream_db # получаем данные: полный список сортов
     icecreams = ''
     for i in range(len(db_query)):
-        icecreams += f'{db_query[i]["name"]}'
-    ice = {'icecream': icecreams}
+        icecreams += f'{db_query[i]["name"]} <br>'
+    context = {'icecreams': icecreams}
 
-    return render(request, 'icecream/icecream-list.html', context=ice)
+    return render(request, 'icecream/icecream-list.html', context)
