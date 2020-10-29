321f67d96133815296579f0a77c2eec96da5ed47
JustLike420 <justlikevova@gmail.com>
2020-10-29 21:37:53 +0300

    added weather

--- homepage/views.py
+++ homepage/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from icecream.models import icecream_db
 from anfis.models import friends_db
+from anfis.services import what_weather
 
 def index(request):
     icecreams = ''
     friends = ''
     for friend in friends_db:
-        friends += f'{friend} <br>'
+        city = friends_db[friend]
+        weather = what_weather(city)
+        friends += f'{friend} :: {city} :: {weather} <br>'
+
     for i in range(len(icecream_db)):
         icecreams += f"{icecream_db[i]['name']} | <a href='icecream/{i}/'>Узнать состав</a> <br>"
+
     context = {
         'icecreams': icecreams,
         'friends': friends,
