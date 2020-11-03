23add4216a1f57a56a4247cf94d1d3f9618f2db5
JustLike420 <justlikevova@gmail.com>
2020-11-03 16:37:25 +0300

    added form for friends adding

--- friends_reg/views.py
+++ friends_reg/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from anfis.models import Friend
 # Create your views here.
 def friends_registr(request):
+    if request.method == 'POST':
+        name_e = request.POST['name']
+        city_e = request.POST['city']
+        b = Friend(name=name_e, city=city_e)
+        b.save()
     return render(request, 'friends_reg/friends.html')
