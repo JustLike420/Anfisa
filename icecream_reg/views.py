05804d9f007fa8c75885c8dd58350c8e9358bcd5
JustLike420 <justlikevova@gmail.com>
2020-11-03 18:07:30 +0300

    added icecream registration form

--- icecream_reg/views.py
+++ icecream_reg/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from icecream.models import Icecream
 # Create your views here.
 def icecream_registr(request):
+    if request.method == 'POST':
+        icecream = request.POST['name']
+        description = request.POST['description']
+        b = Icecream(name=icecream, description=description)
+        b.save()
     return render(request, 'icecream_reg/icecream.html')
